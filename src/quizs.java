ef857e8c69bdce18c91c2d4c0ef8d3a63f2bd6b6
charity <charity23000@student.hamk.fi>
2023-10-09 21:06:07 +0300

    Update quizs.java

--- src/quizs.java
+++ src/quizs.java
@@ -3,9 +3,47 @@ public class quizs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String creator = "Your Name";
+
+        // print printer App
+        System.out.println("Printer App");
+
+        // Step 3
+        System.out.println("This app is made by:");
+
+        // S
+        System.out.print("This app is made by: ");
+        System.out.println(creator);
+
+        // Step 6
+        int number1 = 5;
+        int number2 = 2;
+        // print the value of the number
+        // Step 8
+        System.out.println("The value of number1 is " + number1);
+
+        // adding of numbers
+        System.out.println("The value of number2 is " + number2);
+
+        // Step 10
+        int multiplication = number1 * number2;
+
+        // Step 12
+        System.out.println(number1 + " * " + number2 + " = " + multiplication);
+
+        // Repeat 10-12 for addition, division, and subtraction
+        int addition = number1 + number2;
+        System.out.println(number1 + " + " + number2 + " = " + addition);
+
+        double divisionResult = (double) number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + divisionResult);
+
+        int subtraction = number1 - number2;
+        System.out.println(number1 + " - " + number2 + " = " + subtraction);
+    }
 		
 		
 
 	}
 
-}
+
