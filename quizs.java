6a7dd8de4a9a91af1306403c4bef74f999f13789
charity <charity23000@student.hamk.fi>
2023-10-09 21:12:51 +0300

    Create quizs.java

---
+++ quizs.java
@@ -0,0 +1,17 @@
+
+public class quizs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String creator = "Your Name";
+
+        // Step 2
+        System.out.println("Printer App");
+
+        // Step 3
+        Sys
+	}
+
+}
